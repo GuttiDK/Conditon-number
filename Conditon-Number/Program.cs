e2dc054203d20366da355f3877ebd31c05491625
GuttiDK <93974633+GuttiDK@users.noreply.github.com>
2022-12-07 10:10:20 +0100

    V 1.0.0

---
+++ Conditon-Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Program
+{
+
+    public class Program
+    {
+
+        static void MainStart(string[] args)
+        {
+            Console.Write("input: ");
+            string test = Kondital("Hejsa med dig");
+            Console.WriteLine(test);
+        }
+
+        static string Kondital(string est)
+        {
+            Console.WriteLine(est);
+            return "Hejsa";
+        }
+
+    }
+}
